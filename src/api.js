f6523657e85e056db9d9c3cb07b0416fd6985cfd
abha <abha@example.com>
2022-08-06 19:41:56 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,21 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "93119ce515mshb103c7e0624500dp1b32a6jsnfbc8f92a3376", // enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = ""; // enter your key from openweather API
+
+/*export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "93119ce515mshb103c7e0624500dp1b32a6jsnfbc8f92a3376",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+*/
